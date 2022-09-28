3926f708f3a437cec627a3fc902757bb957f0d42
Martin Tran <>
2022-09-28 18:42:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hi, there! Let's come up with a name for your band.\n")
+
+city = input("\nWhat city did you grow up in?\n")
+
+pet = input("\nWhat is the name of your pet? If you don't have a pet,\ntype in any name you would give a pet.\n")
+
+print("\nYour band name is ~ " + city + " " + pet + " ~")
